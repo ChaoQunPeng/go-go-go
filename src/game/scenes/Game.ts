0cab9fd865df5191da0d52593f12f1091c8ac6e7
ppccqq <1152684231@qq.com>
2026-07-13 22:44:05 +0800

    🎉 init: init

---
+++ src/game/scenes/Game.ts
@@ -0,0 +1,17 @@
+import { Scene } from 'phaser';
+
+export class Game extends Scene {
+    constructor() {
+        super('Game');
+    }
+
+    preload() {
+        this.load.setPath('assets');
+        this.load.image('background', 'bg.png');
+        this.load.image('logo', 'logo.png');
+    }
+
+    create() {
+
+    }
+}
