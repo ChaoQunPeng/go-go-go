1da56b201ac67c43dedbba570d0478fd1dbd44f5
ppccqq <1152684231@qq.com>
2026-07-18 15:16:38 +0800

    ✨ feat: 增加分数系统

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -3,6 +3,7 @@ import { Types, Scene } from 'phaser';
 import { Player } from '../player/player.ts';
 import { PlatformManager } from '../platform/PlatformManager.ts';
 import { RockManager } from '../rock/RockManager.ts';
+import { ScoreManager } from '../score/ScoreManager.ts';
 
 // 定义一个名叫 Game 的场景类，Phaser 会把它当成一个游戏画面来运行。
 export class Game extends Scene {
@@ -10,6 +11,8 @@ export class Game extends Scene {
     private cursors!: Types.Input.Keyboard.CursorKeys;
     private platformManager!: PlatformManager;
     private rockManager!: RockManager;
+    private scoreManager: ScoreManager;
+
     private readonly worldSpeed = 200;
 
     // 构造函数会在创建这个场景时执行一次。
@@ -26,6 +29,7 @@ export class Game extends Scene {
     create() {
         this.player = new Player(this, 100, 300);
         this.rockManager = new RockManager(this, this.player);
+        this.scoreManager = new ScoreManager(this);
         this.platformManager = new PlatformManager(
             this,
             this.player,
@@ -36,6 +40,7 @@ export class Game extends Scene {
 
         this.platformManager.create();
         this.rockManager.create();
+        this.scoreManager.create();
 
         this.initCursors();
     }
@@ -47,7 +52,7 @@ export class Game extends Scene {
         // 先更新已有石头，避免新平台生成的石头在出生帧立即移动。
         this.rockManager.update(scrollDistance);
         this.platformManager.update(scrollDistance);
-
+        this.scoreManager.update(scrollDistance);
         this.player.update(this.cursors);
     }
 
---
+++ src/game/score/ScoreManager.ts
@@ -0,0 +1,35 @@
+import { GameObjects, Scene } from 'phaser';
+
+export class ScoreManager {
+    private score = 0;
+    private scoreText: GameObjects.Text;
+
+    constructor(private scene: Scene) {
+    }
+
+    create() {
+        // 将分数固定在画布顶部中间位置。
+        this.scoreText = this.scene.add
+            .text(this.scene.cameras.main.centerX, 50, '0', {
+                fontSize: '32px',
+                color: '#000000',
+            })
+            .setOrigin(0.5, 0)
+            .setScrollFactor(0)
+            .setDepth(1000);
+    }
+
+    update(scrollDistance: number) {
+        // 每滚动 10 像素增加 1 分。
+        this.score += scrollDistance / 10;
+        this.renderScore();
+    }
+
+    private getScore() {
+        return Math.floor(this.score);
+    }
+
+    private renderScore() {
+        this.scoreText.setText(this.getScore().toString());
+    }
+}
